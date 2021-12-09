bb74b52b0b5f16dae7287125fc6aa2f50f51e621
Natalika-frontend <Natalochka_ne@mail.ru>
2021-12-09 22:58:54 +0300

    wrote two ratings for Ui-kit that work independently of each other

---
+++ src/assets/libs/Ui-Kit/form-elements/switches/rate-buttons/rate-buttons.js
@@ -0,0 +1,79 @@
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+  initRatings();
+}
+
+// основная функция
+function initRatings() {
+  let ratingActive, ratingValue;
+
+  // перебираем все рейтинги на странице
+  for (let index = 0; index < ratings.length; index++) {
+    const rating = ratings[index];
+    initRating(rating);
+    initSecondRating(rating);
+  }
+
+  // инициализируем первый рейтинг
+  function initRating(rating) {
+    initRatingVars(rating);
+    setRatingActiveWidth();
+
+    if (rating.classList.contains('rating_set')) {
+      setRating(rating);
+    }
+  }
+
+  // инициализируем второй рейтинг
+  function initSecondRating(rating) {
+    initRatingVars(rating);
+    setRatingActiveWidth();
+
+    if (rating.classList.contains('rating_second-set')) {
+      setRating(rating);
+    }
+  }
+
+  // инициализируем переменные
+  function initRatingVars(rating) {
+    ratingActive = rating.querySelector('.rating__active');
+    ratingValue = rating.querySelector('.rating__value');
+  }
+
+  // изменяем ширину активных звёзд
+  function setRatingActiveWidth(index = ratingValue.innerHTML) {
+    const ratingActiveWidth = index / 0.05;
+    ratingActive.style.width = `${ratingActiveWidth}%`;
+  }
+
+  // возможность указать оценку
+  function setRating(rating) {
+    const ratingItems = rating.querySelectorAll('.rating__item');
+    for (let index = 0; index < ratingItems.length; index++) {
+      const ratingItem = ratingItems[index];
+      ratingItem.addEventListener("mouseenter", function (e) {
+        // обновление переменных
+        initRatingVars(rating);
+        // обновление активных звёзд
+        setRatingActiveWidth(ratingItem.value);
+      });
+      ratingItem.addEventListener("mouseleave", function (e) {
+        // обновление активных звёзд
+        setRatingActiveWidth();
+      });
+      ratingItem.addEventListener("click", function (e) {
+        // обновление переменных 
+        initRatingVars(rating);
+
+        // отправить на сервер 
+        if (rating.dataset.ajax) {
+          setRatingValue(ratingItem.value, rating);
+        } else {
+          // отобразить указанную оценку
+          ratingValue.innerHTML = index + 1;
+          setRatingActiveWidth();
+        }
+      });
+    }
+  }
+}
--- src/assets/libs/Ui-Kit/mainUi.js
+++ src/assets/libs/Ui-Kit/mainUi.js
@@ -1,3 +1,4 @@
 import './form-elements/switches/checkboxes/checkboxes';
 import './form-elements/switches/radio-buttons/radio-buttons';
-import './form-elements/switches/toggle-buttons/toggle-buttons'
+import './form-elements/switches/toggle-buttons/toggle-buttons';
+import './form-elements/switches/rate-buttons/rate-buttons';
