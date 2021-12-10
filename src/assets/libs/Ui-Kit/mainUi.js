764e007f5a8038fe9e6c0fde86794704950153ca
Natalika-frontend <Natalochka_ne@mail.ru>
2021-12-10 11:11:58 +0300

    added range slider

--- src/assets/libs/Ui-Kit/mainUi.js
+++ src/assets/libs/Ui-Kit/mainUi.js
@@ -2,3 +2,4 @@ import './form-elements/switches/checkboxes/checkboxes';
 import './form-elements/switches/radio-buttons/radio-buttons';
 import './form-elements/switches/toggle-buttons/toggle-buttons';
 import './form-elements/switches/rate-buttons/rate-buttons';
+import './form-elements/switches/range-slider/range-slider';
