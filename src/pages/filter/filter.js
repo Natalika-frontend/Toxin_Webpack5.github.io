cd70cea2b2f3a544f40683ab5b6ab95da73df7cc
Natalika-frontend <Natalochka_ne@mail.ru>
2022-01-14 17:18:37 +0300

    designed filter page

--- src/pages/filter/filter.js
+++ src/pages/filter/filter.js
@@ -1,2 +1,3 @@
 import "../../assets/libs/Ui-Kit/form-elements/buttons-section/range-slider/range-slider";
 import "../../assets/libs/Ui-Kit/form-elements/switches/checkboxes/checkboxes";
+import "../../assets/libs/Ui-Kit/cards/room-cards/room-card";
